846429db17cd30a0fe96867d83bc8e714828d15f
Fanwy2018 <42706148+Fanwy2018@users.noreply.github.com>
2021-06-21 18:04:01 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 所有代码都应该写在这里
     let targetImage = document.querySelector("smart-image");
-    targetImage.classList.add("small");
     targetImage.addEventListener("click", function() {
     alert("click img");
     if (targetImage.classList.contains("small")) {
