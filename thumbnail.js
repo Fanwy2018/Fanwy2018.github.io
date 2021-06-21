b2dbba2a32aec4800e71f7fd4af04df50b94a9d5
Fanwy2018 <42706148+Fanwy2018@users.noreply.github.com>
2021-06-21 17:54:30 +0800

    Add files via upload

---
+++ thumbnail.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 所有代码都应该写在这里
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.classList.add("small");
+    targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+    if (targetImage.classList.contains("small")) {
+  targetImage.classList.remove("small");
+  }
+  else
+  	{targetImage.classList.add("small");}
+});
+});
