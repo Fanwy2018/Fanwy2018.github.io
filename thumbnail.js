124d9299233797356503858ead2be9c7f5a7821b
Fanwy2018 <42706148+Fanwy2018@users.noreply.github.com>
2021-06-21 19:55:59 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var targetImage = document.querySelector("#smart-image");
     targetImage.classList.add("small");
     targetImage.addEventListener("click", function() {
-    //alert("点击了图片");
      if (targetImage.classList.contains("small")) {
     targetImage.classList.remove("small");
 } else {
